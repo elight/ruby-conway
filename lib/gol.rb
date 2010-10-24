18adce4ccfd01875f52d8d242e8167f1e567ff52
Evan Light <evan.light@tripledogdare.net>
2010-10-23 23:30:48 -0400

    Initial

---
+++ lib/gol.rb
@@ -0,0 +1,44 @@
+def run_rule(args = {})
+  state = args[:with_cell_state]
+  neighbor_cnt = args[:and_num_neighbors]
+
+  return 0 unless neighbor_cnt == 3 || 
+                  neighbor_cnt == 2 && state == 1
+  1
+end
+
+def game(initial_state)
+  initial_state.tap do |board|
+    def board.next_state
+      x = 0
+      self.map do |row|
+        y = 0
+        new_row = row.map do |cell|
+          new_state = run_rule(:with_cell_state => self[x][y],
+                               :and_num_neighbors => num_living_neighbors_at(x, y))
+          y += 1
+          new_state
+        end
+        x += 1
+        new_row
+      end
+    end
+
+    def board.num_living_neighbors_at(x, y)
+      result = 0
+      (-1..1).each do |x1|
+        (-1..1).each do |y1|
+          next if (x1 == 0 && y1 == 0) || !is_inbound(x + x1, y + y1)
+          result += 1 if self[x + x1][y + y1] == 1
+        end
+      end
+      result
+    end
+
+    def board.is_inbound(x, y)
+      (x_in_bounds = x >= 0 && x < self.length) &&
+      (y_in_bounds = y >= 0 && y < self[x].length)
+    end
+  end
+end
+
--- test/test_gol.rb
+++ test/test_gol.rb
@@ -1,7 +1,101 @@
 require 'helper'
 
-class TestGol < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class RulesTest < Test::Unit::TestCase
+  def self.assert_process_rule_on(args = {})
+    current_state = args[:cell_that_is]
+    num_neighbors = args[:with_number_of_alive_neighbors]
+    expected_next_state = args[:should_be]
+
+    context "A #{current_state} cell with #{num_neighbors} alive neighbors" do
+      should "be #{expected_next_state}" do
+        next_state = run_rule :with_cell_state => current_state, :and_num_neighbors => num_neighbors
+        assert_equal expected_next_state, next_state
+      end
+    end
+  end
+ 
+  assert_process_rule_on :cell_that_is => 1, 
+                         :with_number_of_alive_neighbors => 1, 
+                         :should_be => 0
+
+  assert_process_rule_on :cell_that_is => 1,
+                         :with_number_of_alive_neighbors => 2, 
+                         :should_be => 1
+
+  assert_process_rule_on :cell_that_is => 1, 
+                         :with_number_of_alive_neighbors => 3, 
+                         :should_be => 1
+
+  assert_process_rule_on :cell_that_is => 1, 
+                         :with_number_of_alive_neighbors => 4, 
+                         :should_be => 0
+
+  assert_process_rule_on :cell_that_is => 0, 
+                         :with_number_of_alive_neighbors => 2, 
+                         :should_be => 0
+
+  assert_process_rule_on :cell_that_is => 0, 
+                         :with_number_of_alive_neighbors => 3, 
+                         :should_be => 1
+end
+
+class GameTest < Test::Unit::TestCase
+  context "A 3x3 game" do
+    setup do
+      @game = game [
+        [1, 1, 0],
+        [1, 0, 0],
+        [0, 0, 0]
+      ]
+    end
+
+    should "be able to transition to its next expected state" do
+      expected = [
+        [1, 1, 0],
+        [1, 1, 0],
+        [0, 0, 0]
+      ]
+      assert_equal expected, @game.next_state
+    end
+
+    [[-1, 0], [0, -1], [3, 0], [0, 3]].each do |coords|
+      should "know that #{coords.inspect} is out of bounds" do
+        assert !@game.is_inbound(*coords)
+      end
+    end
+
+    [[0, 0], [2, 2]].each do |coords|
+      should "know that #{coords.inspect} is out of bounds" do
+        assert @game.is_inbound(*coords)
+      end
+    end
+
+
+    should "know that the number of neighbors at 0,0 is 2" do
+      assert_equal 2, @game.num_living_neighbors_at(0,0)
+    end
+  end
+
+  context "A 5x5 game" do
+    setup do
+      @game = game [
+        [1, 1, 0, 1, 1],
+        [1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 1],
+        [1, 0, 0, 0, 0]
+      ]
+    end
+
+    should "be able to transition to its next expected state" do
+      expected = [
+        [1, 1, 0, 1, 1],
+        [1, 1, 0, 1, 1],
+        [1, 1, 0, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 1, 0, 0, 0]
+      ]
+      assert_equal expected, @game.next_state
+    end
   end
 end
